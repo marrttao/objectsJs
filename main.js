4c4ae63cf71ce057fafad3556a9ee462952cd0a3
marrttao <marrttaoburberry@gmail.com>
2025-09-06 13:27:04 +0300

    completed

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("start!");
+car = {
+    brand: 'Toyota',
+    model: 'Corolla',
+    year: 2020,
+    colors: ['red', 'blue', 'green'],
+}
+
+
+console.log(car.brand + ' ' + car.model + ' '  + car.colors[0]);
+document.getElementById('result').innerHTML = car.brand + ' ' + car.model + ' '  + car.colors[0];
