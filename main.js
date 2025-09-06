5999769c967e7d383ee88a600d0dedbf41f2bc84
marrttao <marrttaoburberry@gmail.com>
2025-09-06 13:37:29 +0300

    completed

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("start!");
+student = {
+    name: 'Mark',
+    age: 21,
+    grade : 'A',
+}
+
+propertyName = 'grade';
+
+console.log(student[propertyName]);
+document.getElementById('result').innerHTML = student[propertyName];
