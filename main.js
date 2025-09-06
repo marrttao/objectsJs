c47b5bb2eb7c6550c869d6ab84ca368245a0e1af
marrttao <marrttaoburberry@gmail.com>
2025-09-06 13:40:41 +0300

    completed

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("start!");
+const product = {
+name: "Ноутбук",
+price: 1200,
+category: "Electronics",
+stock: 30
+};
+
+// aray of properties
+function getProperties(obj) {
+    return Object.keys(obj);
+}
+
+console.log(getProperties(product));
+document.getElementById('result').innerHTML = getProperties(product).join(', ');
+
+
